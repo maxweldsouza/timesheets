2db02fedfc47cb627d4f3d3b92cae3edf6ffb5aa
Maxwel Dsouza <maxellusionist@gmail.com>
2017-03-08 19:19:36 +0530

    Fix calendar day alignment

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -41,7 +41,7 @@ let Calendar = ({
             </div>
         </div>
         <div className='row calendar-weekdays-container'>
-            <div className='columns'>
+            <div>
                 {weekdays.map(day => {
                     return <span key={day} className='calendar-weekday'>
                         {day}
