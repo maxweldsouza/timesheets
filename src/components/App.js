5ff7496b138442e89663b948911f13527bb38546
Maxwel Dsouza <maxellusionist@gmail.com>
2017-03-16 21:15:15 +0530

    Cleaup App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SelectUser from './SelectUser';
 import Calendar from './Calendar';
 import Approval from './Approval';
 
-class App extends Component {
-    render () {
-        return (
-            <div className='row'>
-                <SelectUser />
-                <Calendar month={1}/>
-                <Approval />
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <div className='row'>
+            <SelectUser />
+            <Calendar />
+            <Approval />
+        </div>
+    );
+};
 
 export default App;
